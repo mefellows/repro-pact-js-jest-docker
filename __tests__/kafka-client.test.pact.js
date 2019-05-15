16994a0c008283e497384811d29f70308cfd5a8d
Matt Fellows <matt.fellows@onegeek.com.au>
2019-05-15 14:22:55 +1000

    fix(compose): increase timeout from default 5s

--- __tests__/kafka-client.test.pact.js
+++ __tests__/kafka-client.test.pact.js
@@ -1,34 +1,33 @@
-const path = require('path');
+const path = require("path");
 // const { processMessage } = require('../../src/services/kafka/helpers/index');
 const {
   Matchers,
   MessageConsumerPact,
-  synchronousBodyHandler,
+  synchronousBodyHandler
 } = require("@pact-foundation/pact");
 const { somethingLike: like, term } = Matchers;
 
-const processMessage = (data) => {
+const processMessage = data => {
   return data;
 };
 
 const messagePact = new MessageConsumerPact({
-  consumer: 'some-consumer',
-  dir: path.resolve(__dirname, 'pacts'),
-  pactfileWriteMode: 'update',
-  provider: 'some-provider'
+  consumer: "some-consumer",
+  dir: path.resolve(__dirname, "pacts"),
+  pactfileWriteMode: "update",
+  provider: "some-provider",
+  logLevel: "trace"
 });
 
-describe('receive item', () => {
-  it('accepts a valid item', () => {
-    return (
-      messagePact
-        .expectsToReceive('a valid item record')
-        .withContent({
-          data: {
-            dummy: 1
-          }
-        })
-        .verify(synchronousBodyHandler(processMessage))
-    );
-  });
+describe("receive item", () => {
+  test("accepts a valid item", () => {
+    return messagePact
+      .expectsToReceive("a valid item record")
+      .withContent({
+        data: {
+          dummy: 1
+        }
+      })
+      .verify(synchronousBodyHandler(processMessage));
+  }, 30000);
 });
